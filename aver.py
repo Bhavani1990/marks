0c4d6e932f24b367628e30a07a3be09862f91f75
Bhavani1990 <108508457+Bhavani1990@users.noreply.github.com>
2022-07-26 17:03:40 +0530

    Add files via upload

---
+++ aver.py
@@ -0,0 +1,18 @@
+sub1=int(input("enter sub1 marks:"))
+sub2=int(input("enter sub2 marks:"))
+sub3=int(input("enter sub3 marks:"))
+sub4=int(input("enter sub4 marks:"))
+sub5=int(input("enter sub5 marks:"))
+sum=sub1+sub2+sub3+sub4+sub5
+avg=sum/5
+percentage=(sum/500)*100
+print(sub1)
+print(sub2)
+print(sub3)
+print(sub4)
+print(sub5)
+print(avg)
+print(percentage)
+
+
+               
